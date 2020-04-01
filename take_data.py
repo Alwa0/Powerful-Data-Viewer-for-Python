77abd70c0e26aae9e6883fd4cb7088d5e7290985
Alwa0 <54363667+Alwa0@users.noreply.github.com>
2020-04-01 18:06:26 +0300

    delete unnecessary comments

--- take_data.py
+++ take_data.py
@@ -1,7 +1,6 @@
 import sys
 import pandas
 
-#csv = '/home/alina/IdeaProjects/st/testing_data/d1.csv'
 pandas.options.display.max_columns = None
 pandas.options.display.max_rows = None
 pandas.options.display.width=None
