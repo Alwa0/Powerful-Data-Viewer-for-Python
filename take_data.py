524299f223831f3a92ff95b0739851ba8b4e960d
Alwa0 <54363667+Alwa0@users.noreply.github.com>
2020-04-01 18:05:41 +0300

    remove max columns and rows

--- take_data.py
+++ take_data.py
@@ -1,7 +1,10 @@
 import sys
 import pandas
 
+#csv = '/home/alina/IdeaProjects/st/testing_data/d1.csv'
+pandas.options.display.max_columns = None
+pandas.options.display.max_rows = None
+pandas.options.display.width=None
 csv = sys.argv[1]
-# csv = '/home/alina/IdeaProjects/st/testing_data/d1.csv'
 a = pandas.read_csv(csv)
 print(a)
